ab27eb78aab3757d00c6f0caead77a553db84547
Jamin Thornsberry <jthornsberry@aspirehealthcare.com>
2020-08-25 18:35:36 -0500

    Fix `asdf` operations in `init`

--- init.sh
+++ init.sh
@@ -25,10 +25,10 @@ brew bundle --no-lock --file=config/brew/.Brewfile
 source $(brew --prefix asdf)/asdf.sh
 
 # Add asdf plugins
-cat config/asdf/.tool-versions | cut -d " " -f 1 | xargs -n 1 asdf plugin-add
+cat config/asdf/tools/.tool-versions | cut -d " " -f 1 | xargs -n 1 asdf plugin-add
 
 # Install asdf programs
-(cd config/asdf && NODEJS_CHECK_SIGNATURES=no asdf install)
+(cd config/asdf/tools && NODEJS_CHECK_SIGNATURES=no asdf install)
 
 # Install Fisher
 curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
