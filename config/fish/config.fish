6d452b2bf2f7484e9b2cad1dce605369ccdeb6e1
Jamin Thornsberry <jamin.thorns@gmail.com>
2020-12-03 01:45:06 -0600

    Install `nnn` and `config.fish` changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,20 +13,26 @@ if status is-interactive
     # Disable greeting
     set fish_greeting
 
-    # pisces options
+    # pisces config
     set pisces_only_insert_at_eol 1
+
+    # Editor
+    set -x EDITOR code
+
+    # Pager
+    set -x PAGER less
 
     # less options
     set -x LESS -SRc~
 
-    # Erlang options
+    # Erlang flags
     set -x ERL_AFLAGS -kernel shell_history enabled
 
-    # bat options
+    # bat config
     set -x BAT_PAGER less $LESS
     set -x BAT_THEME Dracula
 
-    # fzf options
+    # fzf config
     set -l fzf_colors "fg:-1,fg+:-1,bg:-1,bg+:-1,hl:6,hl+:6,spinner:4,info:4,marker:4,pointer:2,prompt:2"
     set -x FZF_DEFAULT_OPTS "--reverse --no-info --height 50% --color $fzf_colors"
 end
