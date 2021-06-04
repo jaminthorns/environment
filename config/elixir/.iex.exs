e16b2c9ebf751ff5c2940873eda149ed965ff954
Jamin Thornsberry <jamin.thorns@gmail.com>
2021-06-04 13:05:24 -0500

    Don't bold unnecessarily in prompts

--- config/elixir/.iex.exs
+++ config/elixir/.iex.exs
@@ -3,13 +3,13 @@ prompt = fn alive ->
 
   indicator = green() <> "•"
 
-  left = black() <> "" <> black_background() <> indicator <> " "
+  left = black() <> "" <> bright() <> black_background() <> indicator <> " "
   prefix = blue() <> "%prefix "
   node = if alive, do: cyan() <> "%node ", else: ""
   counter = yellow() <> "%counter"
-  right = " " <> indicator <> default_background() <> black() <> ""
+  right = " " <> indicator <> reset() <> black() <> ""
 
-  bright() <> left <> prefix <> node <> counter <> right <> reset()
+  left <> prefix <> node <> counter <> right <> reset()
 end
 
 IEx.configure(default_prompt: prompt.(false), alive_prompt: prompt.(true))
