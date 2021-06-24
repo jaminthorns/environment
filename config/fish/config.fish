47d945348c3aeca35ab3a8200d40b73778c8e0f7
Jamin Thornsberry <jamin.thorns@gmail.com>
2021-06-24 12:39:00 -0500

    Initialize asdf on all shells, not just login shells

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,9 +2,6 @@ if status is-login
     # Initialize Homebrew
     {{fish -c "brew shellenv"}}
 
-    # Initialize asdf
-    source {{brew --prefix asdf}}/asdf.fish
-
     # Enable global Yarn packages
     set -p PATH {{yarn global bin}}
 end
@@ -36,6 +33,9 @@ if status is-interactive
     set -l fzf_colors "fg:-1,fg+:-1,bg:-1,bg+:-1,hl:6,hl+:6,spinner:4,info:4,marker:4,pointer:2,prompt:2"
     set -x FZF_DEFAULT_OPTS "--reverse --no-info --height 50% --color $fzf_colors"
 end
+
+# Initialize asdf
+source {{brew --prefix asdf}}/asdf.fish
 
 # Secrets
 test -e $HOME/.secrets.fish && source $HOME/.secrets.fish
