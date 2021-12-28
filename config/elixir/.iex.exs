2361950dcc6d8200791052d3dc86ff58bc826012
Jamin Thornsberry <jamin.thorns@gmail.com>
2021-12-28 10:32:07 -0600

    Remove indicators from Elixir and DB cli prompts

--- config/elixir/.iex.exs
+++ config/elixir/.iex.exs
@@ -1,13 +1,11 @@
 prompt = fn alive ->
   import IO.ANSI
 
-  indicator = green() <> "•"
-
-  left = black() <> "" <> bright() <> black_background() <> indicator <> " "
+  left = black() <> "" <> bright() <> black_background() <> " "
   prefix = blue() <> "%prefix "
   node = if alive, do: cyan() <> "%node ", else: ""
   counter = yellow() <> "%counter"
-  right = " " <> indicator <> reset() <> black() <> ""
+  right = " " <> reset() <> black() <> ""
 
   left <> prefix <> node <> counter <> right <> reset()
 end
