69484ba7a502e7af330aa3fcbe85f9ee4bfa06e9
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-02-13 00:25:58 -0600

    Fix git fzf functions

--- config/fish/functions/_git_stashes.fish
+++ config/fish/functions/_git_stashes.fish
@@ -1,5 +1,5 @@
 function _git_stashes
-  set preview_command "echo {} | cut -d ':' -f 1 | xargs -I _ git stash show --patch _ | delta --width \$FZF_PREVIEW_COLUMNS"
+  set preview_command "echo {} | cut -d ':' -f 1 | xargs -I [] git stash show --patch [] | delta --width \$FZF_PREVIEW_COLUMNS"
 
   git stash list | fzf --no-sort --preview $preview_command | cut -d ':' -f 1
 end
