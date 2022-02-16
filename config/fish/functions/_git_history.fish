a3d5c2f762c565038634a98aae94a7865dece695
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-02-15 21:55:39 -0600

    Enhance `git history` and `git stashes`

--- config/fish/functions/_git_history.fish
+++ config/fish/functions/_git_history.fish
@@ -6,7 +6,9 @@ function _git_history
     end
 
     set commit_pattern "[0-9a-f]+"
-    set preview_command "echo {} | grep -oE $commit_pattern | head -1 | xargs -I [] git show --color [] $file | delta --width \$FZF_PREVIEW_COLUMNS"
+    set commit_command "echo {} | grep -oE $commit_pattern | head -1"
+    set view_command "$commit_command | xargs -I [] git show --color [] $file"
+    set preview_command "$view_command | delta --width \$FZF_PREVIEW_COLUMNS"
 
-    git graph --color $argv $_flag_file | fzf --ansi --no-sort --preview $preview_command | grep -oE $commit_pattern | head -1
+    git graph --color $argv $_flag_file | fzf --ansi --no-sort --preview="$preview_command" --bind="alt-enter:execute($view_command)" --bind="enter:execute($commit_command)+abort"
 end
--- config/fish/functions/_git_stashes.fish
+++ config/fish/functions/_git_stashes.fish
@@ -1,5 +1,7 @@
 function _git_stashes
-  set preview_command "echo {} | cut -d ':' -f 1 | xargs -I [] git stash show --patch [] | delta --width \$FZF_PREVIEW_COLUMNS"
+    set commit_command "echo {} | cut -d ':' -f 1"
+    set view_command "$commit_command | xargs -I [] git stash show --patch []"
+    set preview_command "$view_command | delta --width \$FZF_PREVIEW_COLUMNS"
 
-  git stash list | fzf --no-sort --preview $preview_command | cut -d ':' -f 1
+    git stash list | fzf --no-sort --preview="$preview_command" --bind="alt-enter:execute($view_command)" --bind="enter:execute($commit_command)+abort"
 end
