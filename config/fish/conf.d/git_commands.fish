5b138fb7e066cc3e186b20dfce3301fbe23c6aff
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-05-06 11:07:43 -0500

    Add view options configuration for git commands

--- config/fish/conf.d/git_commands.fish
+++ config/fish/conf.d/git_commands.fish
@@ -1,9 +1,15 @@
 complete -c git-branches -w "git branch"
-complete -c git-compare -w "git diff"
-complete -c git-history -w "git log"
-complete -c git-stashes -w "git stash"
-complete -c git-view -w "git show"
+complete -c git-branches -s o -l graph-options -d "Options for the 'git graph' view"
 
+complete -c git-compare -w "git diff"
 complete -c git-compare -s p -l path -d "Show only changes from specified path"
+
+complete -c git-history -w "git log"
 complete -c git-history -s p -l path -d "Show only commits and changes from specified path"
+complete -c git-history -s o -l show-options -d "Options for the 'git show' view"
+
+complete -c git-stashes -w "git stash"
+complete -c git-history -s o -l stash-show-options -d "Options for the 'git stash show' view"
+
+complete -c git-view -w "git show"
 complete -c git-view -s p -l path -d "Show only changes from specified path"
