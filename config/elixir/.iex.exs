f7b6347571a41365beaff5e515b94ba1575bf032
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-05-10 01:56:12 -0500

    Prompt tweaks

--- config/elixir/.iex.exs
+++ config/elixir/.iex.exs
@@ -1,13 +1,17 @@
 prompt = fn alive ->
   import IO.ANSI
 
+  separator = light_black() <> " ╱ "
+
   left = black() <> "" <> bright() <> black_background() <> " "
-  prefix = blue() <> "%prefix "
-  node = if alive, do: cyan() <> "%node ", else: ""
+  prefix = blue() <> "%prefix"
+  node = if alive, do: cyan() <> "%node"
   counter = yellow() <> "%counter"
   right = " " <> reset() <> black() <> ""
 
-  left <> prefix <> node <> counter <> right <> reset()
+  segments = Enum.reject([prefix, node, counter], &is_nil/1)
+
+  left <> Enum.join(segments, separator) <> right <> reset()
 end
 
 IEx.configure(default_prompt: prompt.(false), alive_prompt: prompt.(true))
