16d35a95d936d61727735f80e703d8ac06220a74
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-05-16 12:57:24 -0500

    Configure Tide prompt on init

--- init.sh
+++ init.sh
@@ -33,6 +33,9 @@ source push.sh
 # Install Fisher and packages
 fish -c "curl -sL https://git.io/fisher | source && fisher update"
 
+# Configure Tide prompt
+fish -c "tide configure <(string join '' 2 2 1 3 3 4 1 1 1 y | psub) 1>/dev/null"
+
 # Get fish path
 fish=$(command -v fish)
 
