cae987b8f2f41d0ec849ed6a18bc09dac69c38ba
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-05-27 01:06:58 -0500

    Set true color support and improve comments in fish config

--- config/fish/config/config.fish
+++ config/fish/config/config.fish
@@ -7,30 +7,33 @@ if status is-interactive
     # Disable greeting
     set fish_greeting
 
-    # Editor
+    # Set editor to Visual Studio Code
     set -x EDITOR code
 
-    # less options
+    # Configure less
     set -x LESS "--RAW-CONTROL-CHARS --use-color --ignore-case --chop-long-lines --clear-screen --tilde --shift=.1"
 
-    # Pager
+    # Set pager to less
     set -x PAGER less $LESS
 
-    # Erlang flags
+    # Explicitly set true color support
+    set -x COLORTERM truecolor
+
+    # Configure Erlang
     set -x ERL_AFLAGS -kernel shell_history enabled
 
-    # bat config
+    # Configure bat
     set -x BAT_PAGER $PAGER
     set -x BAT_THEME Dracula
 
-    # fzf config
+    # Configure fzf
     set -l fzf_colors "bg+:#44475A,gutter:-1,hl:cyan,hl+:cyan,info:blue,marker:blue,pointer:blue,prompt:green,border:blue,header:bright-black"
     set -x FZF_DEFAULT_OPTS "--reverse --no-info --preview-window=border-sharp --prompt='▶ ' --pointer='▶' --marker='•' --color=$fzf_colors"
 
-    # fzf.fish keybindings
+    # Configure fzf.fish keybindings
     fzf_configure_bindings --directory=\cT --history=\cR --variables=\cB --processes=\cG --git_status --git_log
 
-    # Tide prompt config
+    # Customize Tide prompt
     set -x tide_status_icon ✓
     set -x tide_status_icon_failure ✕
 end
