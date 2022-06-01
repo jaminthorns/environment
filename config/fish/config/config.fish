b442b20bafe9968f87f5555ac99bd4a1b7924a54
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-06-01 10:10:01 -0500

    Explicit scoping in fish config

--- config/fish/config/config.fish
+++ config/fish/config/config.fish
@@ -5,40 +5,40 @@ end
 
 if status is-interactive
     # Disable greeting
-    set fish_greeting
+    set -g fish_greeting
 
     # Enable alternate scroll mode
     printf "\e[?1007h"
 
     # Set editor to Visual Studio Code
-    set -x EDITOR code
+    set -gx EDITOR code
 
     # Configure less
-    set -x LESS "--RAW-CONTROL-CHARS --use-color --ignore-case --chop-long-lines --clear-screen --tilde --shift=.1"
+    set -gx LESS "--RAW-CONTROL-CHARS --use-color --ignore-case --chop-long-lines --clear-screen --tilde --shift=.1"
 
     # Set pager to less
-    set -x PAGER less $LESS
+    set -gx PAGER less $LESS
 
     # Set true color support
-    set -x COLORTERM truecolor
+    set -gx COLORTERM truecolor
 
     # Configure Erlang
-    set -x ERL_AFLAGS -kernel shell_history enabled
+    set -gx ERL_AFLAGS -kernel shell_history enabled
 
     # Configure bat
-    set -x BAT_PAGER $PAGER
-    set -x BAT_THEME Dracula
+    set -gx BAT_PAGER $PAGER
+    set -gx BAT_THEME Dracula
 
     # Configure fzf
     set -l fzf_colors "bg+:#44475A,gutter:-1,hl:cyan,hl+:cyan,info:blue,marker:blue,pointer:blue,prompt:green,border:blue,header:bright-black"
-    set -x FZF_DEFAULT_OPTS "--reverse --no-info --preview-window=border-sharp --prompt='▶ ' --pointer='▶' --marker='•' --color=$fzf_colors"
+    set -gx FZF_DEFAULT_OPTS "--reverse --no-info --preview-window=border-sharp --prompt='▶ ' --pointer='▶' --marker='•' --color=$fzf_colors"
 
     # Configure fzf.fish keybindings
     fzf_configure_bindings --directory=\cT --history=\cR --variables=\cB --processes=\cG --git_status --git_log
 
     # Customize Tide prompt
-    set -x tide_status_icon ✓
-    set -x tide_status_icon_failure ✕
+    set -gx tide_status_icon ✓
+    set -gx tide_status_icon_failure ✕
 end
 
 # Initialize asdf
