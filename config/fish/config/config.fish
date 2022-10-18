038d4b2f477fb8eda5ff2b595b7de01e339b1271
Jamin Thornsberry <jamin.thorns@gmail.com>
2022-10-18 13:04:33 -0500

    Tweak some `less` options

--- config/fish/config/config.fish
+++ config/fish/config/config.fish
@@ -14,7 +14,7 @@ if status is-interactive
     set -gx EDITOR code
 
     # Configure less
-    set -gx LESS "--RAW-CONTROL-CHARS --use-color --ignore-case --chop-long-lines --clear-screen --tilde --shift=.1"
+    set -gx LESS --status-column --RAW-CONTROL-CHARS --use-color --ignore-case --chop-long-lines --clear-screen --tilde --shift=.1
 
     # Set pager to less
     set -gx PAGER less $LESS
@@ -31,7 +31,7 @@ if status is-interactive
 
     # Configure fzf
     set -l fzf_colors "bg+:#44475A,gutter:-1,hl:cyan,hl+:cyan,info:blue,marker:blue,pointer:blue,prompt:green,border:blue,header:bright-black"
-    set -gx FZF_DEFAULT_OPTS "--reverse --no-info --preview-window=border-sharp --prompt='▶ ' --pointer='▶' --marker='•' --color=$fzf_colors"
+    set -gx FZF_DEFAULT_OPTS --reverse --no-info --preview-window=border-sharp --prompt="'▶ '" --pointer=▶ --marker=• --color=$fzf_colors
 
     # Configure fzf.fish keybindings
     fzf_configure_bindings --directory=\cT --history=\cR --variables=\cB --processes=\cG --git_status --git_log
