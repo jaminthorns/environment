02e7fba579412c72d2f3b8976c8bae58f7ad4673
Jamin Thornsberry <jamin.thorns@gmail.com>
2023-03-20 17:04:04 -0500

    Fall back to `bat` for file previews

--- config/fish/config/functions/preview.fish
+++ config/fish/config/functions/preview.fish
@@ -18,12 +18,7 @@ function preview
             tiv $tiv_options $path 2>/dev/null
         case 'video/*'
             tiv $tiv_options (ffmpeg -v quiet -i $path -frames:v 1 -f image2 - | psub) 2>/dev/null
-        case application/json
-            bat --style=numbers --color=always $path
-        case 'text/*'
-            bat --style=numbers --color=always $path
         case '*'
-            echo Unsupported file type
-            return 1
+            bat --plain --color=always $path
     end
 end
