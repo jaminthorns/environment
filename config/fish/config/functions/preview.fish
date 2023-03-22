c91bda76d083cdabcb57ebe6024b1a84d4281af4
Jamin Thornsberry <jamin.thorns@gmail.com>
2023-03-21 22:05:54 -0500

    Replace `tiv` with `chafa`

--- config/fish/config/functions/preview.fish
+++ config/fish/config/functions/preview.fish
@@ -1,24 +1,29 @@
 function preview
-    argparse "width=" "height=" -- $argv
+    argparse embedded "width=" "height=" -- $argv
 
     set path $argv
+    set chafa_options --color-extractor=median --bg="#282A36"
 
-    set tiv_options -0
+    if set -q _flag_embedded
+        set -a chafa_options --format=symbols --animate=off
+    else
+        set -a chafa_options --format=iterm
+    end
 
     if set -q _flag_width _flag_height
-        set -a tiv_options -w $_flag_width -h $_flag_height
+        set -a chafa_options --size={$_flag_width}x{$_flag_height}
     end
 
     switch (file -b --mime-type $path)
         case inode/directory
             exa --all --oneline $path
         case application/pdf
-            tiv $tiv_options (pdftoppm -f 1 -l 1 $path | psub) 2>/dev/null
-        case 'image/*'
-            tiv $tiv_options $path 2>/dev/null
-        case 'video/*'
-            tiv $tiv_options (ffmpeg -v quiet -i $path -frames:v 1 -f image2 - | psub) 2>/dev/null
-        case '*'
+            pdftoppm -jpeg -f 1 -l 1 $path | chafa $chafa_options
+        case "image/*"
+            chafa $chafa_options $path 2>/dev/null || magick $path tiff:- | chafa $chafa_options
+        case "video/*"
+            ffmpeg -v quiet -i $path -frames:v 1 -codec:v tiff -f image2pipe - | chafa $chafa_options
+        case "*"
             bat --plain --color=always $path
     end
 end
