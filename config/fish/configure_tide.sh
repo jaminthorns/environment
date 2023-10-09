81a24fb6ab018d869314e1e44d40c709984311fc
Jamin Thornsberry <jamin.thorns@gmail.com>
2023-10-09 09:55:35 -0500

    Upgrade Tide to v6

--- config/fish/configure_tide.sh
+++ config/fish/configure_tide.sh
@@ -1,1 +1,11 @@
-fish -c "echo 2 2 1 3 3 4 1 1 1 y | tide configure >/dev/null"
+fish -c "tide configure --auto \
+  --style='Classic' \
+  --prompt_colors='16 colors' \
+  --show_time='No' \
+  --classic_prompt_separators='Slanted' \
+  --powerline_prompt_heads='Slanted' \
+  --powerline_prompt_tails='Slanted' \
+  --powerline_prompt_style='One line' \
+  --prompt_spacing='Compact' \
+  --icons='Few icons' \
+  --transient='No'" > /dev/null
