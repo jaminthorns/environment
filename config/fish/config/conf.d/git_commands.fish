ecdd71b339f5984b3d5aa5394db66134af5d2db5
Jamin Thornsberry <jamin.thorns@gmail.com>
2023-10-24 11:29:06 -0500

    Enhance `git stashes` command
    This show stashes with `git show` instead of `git stash show`, so you get message and parent information. Also, change the `-o` option in `git history` and `git stashes` to `-s` (it's a better mnemonic).

--- config/fish/config/conf.d/git_commands.fish
+++ config/fish/config/conf.d/git_commands.fish
@@ -7,10 +7,10 @@ complete -c git-compare -s p -l path -d "Show only changes from specified path"
 complete -c git-history -w "git log"
 complete -c git-history -s p -l path -d "Show only commits and changes from specified path"
 complete -c git-history -s f -l file -d "Show only commits and changes from specified file"
-complete -c git-history -s o -l show-options -d "Options for the 'git show' view"
+complete -c git-history -s s -l show-options -d "Options for the 'git show' view"
 
 complete -c git-stashes -w "git stash"
-complete -c git-stashes -s o -l stash-show-options -d "Options for the 'git stash show' view"
+complete -c git-stashes -s s -l show-options -d "Options for the 'git show' view"
 
 complete -c git-view -w "git show"
 complete -c git-view -s p -l path -d "Show only changes from specified path"
