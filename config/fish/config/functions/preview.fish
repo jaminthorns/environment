8079cc433a8fc3ddecc77b0e6fbbb3df32efb214
Jamin Thornsberry <jamin.thorns@gmail.com>
2024-01-19 16:37:31 -0600

    Using `timg` to try and get image previews in terminal working again

--- config/fish/config/functions/preview.fish
+++ config/fish/config/functions/preview.fish
@@ -4,19 +4,20 @@ function preview
     argparse "width=" "height=" -- $argv
 
     set path $argv
-    set chafa_options --animate=off --bg="#282A36"
+    set timg_options --frames=1 -b "#282A36"
 
     set -e options[(contains -i $path $options)]
 
     switch $TERM_PROGRAM
         case WezTerm
-            set -a chafa_options --format=sixels
+            set -a timg_options --pixelation=sixel
         case vscode
-            set -a chafa_options --format=symbols --color-extractor=median
+            # set -a timg_options --pixelation=quarter
+            set -a timg_options --pixelation=sixel
     end
 
     if set -q _flag_width _flag_height
-        set -a chafa_options --size={$_flag_width}x{$_flag_height}
+        set -a timg_options -g {$_flag_width}x{$_flag_height}
     end
 
     if not test -s $path
@@ -29,11 +30,11 @@ function preview
         case application/gzip application/x-7z-compressed application/x-bzip2 application/x-tar application/x-xz application/zip application/zstd
             ouch list --yes --tree $path
         case application/pdf
-            pdftoppm -jpeg -f 1 -l 1 $path | chafa $chafa_options
+            pdftoppm -jpeg -f 1 -l 1 $path | timg $timg_options
         case "image/*"
-            chafa $chafa_options $path 2>/dev/null || magick $path tiff:- | chafa $chafa_options
+            timg $timg_options $path 2>/dev/null || magick $path tiff:- | timg $timg_options
         case "video/*"
-            ffmpeg -v quiet -i $path -frames:v 1 -codec:v tiff -f image2pipe - | chafa $chafa_options
+            ffmpeg -v quiet -i $path -frames:v 1 -codec:v tiff -f image2pipe - | timg $timg_options
         case "*"
             if test (file -b --mime-encoding $path) = binary
                 hexyl --border=none $path
