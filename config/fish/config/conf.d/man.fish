e6958fd748b18a83767cf190001b40848fe52656
Jamin Thornsberry <jamin.thorns@gmail.com>
2024-04-11 10:30:06 -0500

    Make sure `man` function doesn't override base `man` function
    I didn't realize Fish had a built-in wrapper for this.

--- config/fish/config/conf.d/man.fish
+++ config/fish/config/conf.d/man.fish
@@ -1,8 +1,10 @@
+functions -c man __man
+
 function man --wraps man
     set -x LESS_TERMCAP_md (set_color -o blue) # Start bold
     set -x LESS_TERMCAP_me (set_color normal) # End bold
     set -x LESS_TERMCAP_us (set_color -u cyan) # Start underline
     set -x LESS_TERMCAP_ue (set_color normal) # End underline
 
-    command man $argv
+    __man $argv
 end
--- config/fish/config/conf.d/set_remote.fish
+++ config/fish/config/conf.d/set_remote.fish
@@ -3,17 +3,17 @@ function parent_pid -a pid
 end
 
 function set_remote -a pid name
-    set process_var (_set_remote_process_var $pid)
+    set process_var (__set_remote_process_var $pid)
     set values_var (string join _ $process_var $name)
     set -U -a $process_var $name
     set -U $values_var $argv[3..]
 end
 
-function _set_remote_process_var -a pid
+function __set_remote_process_var -a pid
     echo _set_remote_$pid
 end
 
-function _set_remote_receive -a type event process_var --on-variable (_set_remote_process_var $fish_pid)
+function __set_remote_receive -a type event process_var --on-variable (__set_remote_process_var $fish_pid)
     if test $event = SET && set -q $process_var
         for name in $$process_var
             set values_var (string join _ $process_var $name)
