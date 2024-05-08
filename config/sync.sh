731a3ea85e9b45cd31e7754f411baf42f48c232a
Jamin Thornsberry <jamin.thorns@gmail.com>
2024-05-08 09:50:55 -0500

    Uninstall Continue extension
    It's been hogging the extension host and causing all the other extensions to hang.

--- config/sync.sh
+++ config/sync.sh
@@ -21,14 +21,12 @@ if os_status linux-non-wsl; then
 fi
 
 if os_status mac-os || os_status linux-non-wsl; then
-  copy continue $HOME/.continue
   copy wezterm $HOME/.config/wezterm
 fi
 
 if os_status wsl; then
   copy autohotkey "$APPDATA/Microsoft/Windows/Start Menu/Programs/Startup"
   copy code/settings "$APPDATA/Code/User"
-  copy continue "$USERPROFILE/.continue"
   copy wezterm "$ProgramFiles/WezTerm"
 fi
 
