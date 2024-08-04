67619cd939e4f97c7260e115e74601e187fb85d8
Jamin Thornsberry <jamin.thorns@gmail.com>
2024-08-04 15:32:58 -0500

    Rename `linux_dependencies` -> `linux_packages`

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ source functions/read_config.sh
 source functions/config_functions.sh
 
 # Install Linux dependencies
-os_status linux && sudo apt update && sudo apt upgrade -y && find config/linux_dependencies -type f | xargs cat | xargs sudo apt install -y
+os_status linux && sudo apt update && sudo apt upgrade -y && find config/linux_packages -type f | xargs cat | xargs sudo apt install -y
 
 # Get Homebrew path
 os_status linux && brew="/home/linuxbrew/.linuxbrew/bin/brew" || brew="/opt/homebrew/bin/brew"
