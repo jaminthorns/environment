298a1e453fac9554f5f8a6d272bdf8a34ef2ebd0
Jamin Thornsberry <jamin.thorns@gmail.com>
2025-09-05 15:37:10 -0500

    Clean up `fzf` config

--- config/fish/config/config.fish
+++ config/fish/config/config.fish
@@ -52,19 +52,37 @@ if status is-interactive
     end
 
     # Configure fzf
-    set -l scrollbar_color "#363848" # Selection color blended 50% with background color
-    set -l fzf_colors "bg+:#44475A,gutter:-1,hl:cyan,hl+:cyan,info:blue,marker:magenta,spinner:green,prompt:green,border:blue,scrollbar:$scrollbar_color,preview-scrollbar:$scrollbar_color,header:bright-black"
-    set -gx FZF_DEFAULT_OPTS --reverse --info=inline-right --no-separator --highlight-line --preview-window=border-left --scrollbar=█ \
-        --bind=esc:cancel \
-        --bind=alt-p:toggle-preview \
-        --bind=alt-up:first \
-        --bind=alt-down:last \
+    set -l fzf_scrollbar_color "#363848" # Selection color blended 50% with background color
+    set -l fzf_colors \
+        "bg+:#44475A" \
+        "gutter:-1" \
+        "hl:cyan" \
+        "hl+:cyan" \
+        "info:blue" \
+        "marker:magenta" \
+        "spinner:green" \
+        "prompt:green" \
+        "border:blue" \
+        "scrollbar:$fzf_scrollbar_color" \
+        "preview-scrollbar:$fzf_scrollbar_color" \
+        "header:bright-black"
+    set -gx FZF_DEFAULT_OPTS \
+        --reverse \
+        --info=inline-right \
+        --no-separator \
+        --highlight-line \
+        --preview-window=border-left \
         --prompt=(string escape "▶ ") \
         --pointer= \
         --marker=▌ \
         --marker-multi-line=▌▌▌ \
         --ellipsis=… \
-        --color=$fzf_colors
+        --scrollbar=█ \
+        --bind=esc:cancel \
+        --bind=alt-p:toggle-preview \
+        --bind=alt-up:first \
+        --bind=alt-down:last \
+        --color=(string join , $fzf_colors)
 
     # Configure fzf.fish
     set -g fzf_history_time_format "%F %r"
