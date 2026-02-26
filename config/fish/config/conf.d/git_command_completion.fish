0d33e915f3e305bad6e9a79913669fa040eb0b85
Jamin Thornsberry <jamin.thorns@gmail.com>
2026-02-26 15:29:30 -0600

    Remove unused git command completions
    These should have been removed in 10144e9ae46f3bec1e5de4bc8dabab0ec461813c.

--- config/fish/config/conf.d/git_command_completion.fish
+++ config/fish/config/conf.d/git_command_completion.fish
@@ -10,18 +10,14 @@ complete -c git-branches -w "git branch"
 complete -c git-branches -s l -l log-options -d "Options for the 'git log' view"
 
 complete -c git-compare -w "git diff"
-complete -c git-compare -s p -l path -d "Show only changes from specified path"
 
 complete -c git-history -w "git log"
-complete -c git-history -s p -l path -d "Show only commits and changes from specified path"
-complete -c git-history -s f -l file -d "Show only commits and changes from specified file"
 complete -c git-history -s s -l show-options -d "Options for the 'git show' view"
 
 complete -c git-stashes -w "git stash list"
 complete -c git-stashes -s s -l show-options -d "Options for the 'git show' view"
 
 complete -c git-view -w "git show"
-complete -c git-view -s p -l path -d "Show only changes from specified path"
 
 complete -c git-worktrees -w "git worktree list"
 complete -c git-worktrees -s s -l status-options -d "Options for the 'git status' view"
