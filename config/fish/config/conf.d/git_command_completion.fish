b1393f6113a6d200ed17fc5a624293c40a28ba13
Jamin Thornsberry <jamin.thorns@gmail.com>
2026-02-26 17:08:53 -0600

    Rename interactive Git commands with `i` prefix
    This is clearer and more consistent

--- config/fish/config/conf.d/git_command_completion.fish
+++ config/fish/config/conf.d/git_command_completion.fish
@@ -6,18 +6,18 @@ complete -c git-sprout -k -n "__fish_is_first_arg" -a "(__fish_git_refs)"
 
 complete -c git-purge-branch -k -n "__fish_is_first_arg" -a "(__fish_git_refs)"
 
-complete -c git-branches -w "git branch"
-complete -c git-branches -s l -l log-options -d "Options for the 'git log' view"
+complete -c git-ibranch -w "git branch"
+complete -c git-ibranch -s l -l log-options -d "Options for the 'git log' view"
 
-complete -c git-compare -w "git diff"
+complete -c git-idiff -w "git diff"
 
-complete -c git-history -w "git log"
-complete -c git-history -s s -l show-options -d "Options for the 'git show' view"
+complete -c git-ilog -w "git log"
+complete -c git-ilog -s s -l show-options -d "Options for the 'git show' view"
 
-complete -c git-stashes -w "git stash list"
-complete -c git-stashes -s s -l show-options -d "Options for the 'git show' view"
+complete -c git-ishow -w "git show"
 
-complete -c git-view -w "git show"
+complete -c git-istash -w "git stash list"
+complete -c git-istash -s s -l show-options -d "Options for the 'git show' view"
 
-complete -c git-worktrees -w "git worktree list"
-complete -c git-worktrees -s s -l status-options -d "Options for the 'git status' view"
+complete -c git-iworktree -w "git worktree list"
+complete -c git-iworktree -s s -l status-options -d "Options for the 'git status' view"
