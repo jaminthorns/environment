dd0646975c41f1ffdbfd5e28003c8c6e3ade6e48
Jamin Thornsberry <jamin.thorns@gmail.com>
2026-05-24 11:41:49 -0500

    Fix `man` output coloring in Linux

--- config/fish/config/conf.d/colored_man.fish
+++ config/fish/config/conf.d/colored_man.fish
@@ -3,6 +3,9 @@
 functions -c man __man
 
 function man --wraps man
+    # Disable SGR output in groff so LESS_TERMCAP_* variables work
+    set -x GROFF_NO_SGR 1
+
     set -x LESS_TERMCAP_md (set_color -o blue) # Start bold
     set -x LESS_TERMCAP_me (set_color normal) # End bold
     set -x LESS_TERMCAP_us (set_color -u cyan) # Start underline
