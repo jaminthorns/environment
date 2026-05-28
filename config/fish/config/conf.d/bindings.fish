e148bc75d60ced340570b008e191e26c5b3e16f6
Jamin Thornsberry <jamin.thorns@gmail.com>
2026-05-28 13:44:57 -0500

    Fish keybindings for token movement/deletion

--- config/fish/config/conf.d/bindings.fish
+++ config/fish/config/conf.d/bindings.fish
@@ -4,8 +4,14 @@
 bind ctrl-home beginning-of-buffer
 bind ctrl-end end-of-buffer
 
-# Make alt behavior uniform across operating systems
+# Word movement and deletion
 bind alt-left backward-word
 bind alt-right forward-word
 bind alt-backspace backward-kill-word
 bind alt-delete kill-word
+
+# Token movement and deletion
+bind alt-shift-left backward-token
+bind alt-shift-right forward-token
+bind alt-shift-backspace backward-kill-token
+bind alt-shift-delete kill-token
