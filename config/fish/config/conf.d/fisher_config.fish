89d37bb53fe32f1feb566a498dd6dc2fb17f9f70
Jamin Thornsberry <jamin.thorns@gmail.com>
2026-06-19 11:18:08 -0500

    Use `$HOME` of `~`
    Consistency is key.

--- config/fish/config/conf.d/fisher_config.fish
+++ config/fish/config/conf.d/fisher_config.fish
@@ -5,7 +5,7 @@ set fisher_path $__fish_config_dir/fisher
 set -p fish_complete_path $fisher_path/completions
 set -p fish_function_path $fisher_path/functions
 
-if not test -L ~/.config/fish/themes
+if not test -L $HOME/.config/fish/themes
     ln -s $fisher_path/themes $__fish_config_dir/themes
 end
 
